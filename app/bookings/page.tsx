72d155360aa17d6915af013180e21c132e5791cb
Hamza Rashid <hamzarashid720@gmail.com>
2024-03-30 22:20:53 +0000

    updated inline typeform

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,15 +1,11 @@
-"use client"
-
 import Script from 'next/script';
 import { Widget } from '@typeform/embed-react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-
 const BookingPage = () => {
   return (
     <>
-      {/* Google Tag Manager - Global site tag (gtag.js) */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-16477682494"
         strategy="afterInteractive"
@@ -28,7 +24,11 @@ const BookingPage = () => {
           `,
         }}
       />
-      <Widget id="SAD6oNmE" style={{ width: '100%', height: '100vh' }} />
+      <Widget 
+        id="SAD6oNmE" 
+        style={{ width: '100%', height: '100vh' }} 
+        inlineOnMobile={true} 
+      />
     </>
   );
 };
