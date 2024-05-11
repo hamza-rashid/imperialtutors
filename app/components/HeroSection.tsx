537345dcfcc9d7daacc375679089174b03663d53
Hamza Rashid <hamzarashid720@gmail.com>
2024-05-11 22:31:02 +0100

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -102,7 +102,7 @@ import {
               </Stack>
               <Box textAlign={{ base: 'center', sm: 'unset' }}> 
               <Text fontSize={'lg'} transform={'rotate(10deg)'} marginBottom={{ base: 7, sm: 'none' }}>
-                <Text as="span" fontWeight="bold">£20</Text>/lesson
+                <Text as="span" fontWeight="bold">£15</Text> per hour
               </Text>
                 <Icon
                   as={Arrow}
