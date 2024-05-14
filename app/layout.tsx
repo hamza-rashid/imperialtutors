e288f6127ae2daebcfc7b8f3a7c0ddecc04311fa
Hamza Rashid <hamzarashid720@gmail.com>
2024-05-14 12:37:03 +0100

    updated metadata & opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import { Analytics } from '@vercel/analytics/react';
-import './/globals.css';
+import './globals.css';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'GCSE Doctor',
@@ -18,12 +17,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/images/graphic.png" />
+        <meta property="og:url" content="https://www.gcsedoctor.co.uk" />
+        <meta property="og:type" content="website" />
+      </head>
       <body className={inter.className}>
         <Providers>
-            {children}
-            <Analytics />
+          {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
-  )
+  );
 }
