51ac2a31188e80fcf38f79109fc2485147885d86
Hamza Rashid <hamzarashid720@gmail.com>
2024-11-14 17:53:43 +0000

    Update about link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -64,7 +64,7 @@ export default function Footer() {
             objectFit="contain"
           />
           <Stack direction={'row'} spacing={6}>
-            <Box as="a" href={'/about'}>
+            <Box as="a" href={'https://blog.gcsedoctor.co.uk/about/'}>
               About
             </Box>
             <Box as="a" href={'/reviews'}>
