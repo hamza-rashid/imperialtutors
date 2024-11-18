b224cd73939260c755b73bb01f07e06ae86d5dd6
Hamza Rashid <hamzarashid720@gmail.com>
2024-11-18 22:39:56 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
+    trailingSlash: true, // Ensures that URLs have a trailing slash
     async rewrites() {
       return [
         {
-          source: "/articles/:path*/",
-          destination: "https://articles.gcsedoctor.co.uk/:path*/",
+          source: "/blog/:path*/", // Handles trailing slash versions
+          destination: "https://gcse-doctor.ghost.io/blog/:path*/",
         },
         {
-          source: "/articles/:path*",
-          destination: "https://articles.gcsedoctor.co.uk/:path*",
-        }
+          source: "/blog/:path*", // Handles non-trailing slash versions
+          destination: "https://gcse-doctor.ghost.io/blog/:path*",
+        },
       ];
     },
     async headers() {
       return [
         {
-          source: "/articles/:path*",
+          source: "/blog/:path*", // Adds custom headers to blog requests
           headers: [
-            { key: "x-forwarded-host", value: "gcsedoctor.co.uk" },
+            {
+              key: "x-forwarded-host",
+              value: "articles.gcsedoctor.co.uk", // Replace with your desired subdomain
+            },
           ],
-        }
+        },
       ];
     },
   };
