9b1a130036d85b1e4b1cf7fe6b6b4849b2113493
Hamza Rashid <hamzarashid720@gmail.com>
2024-11-18 22:19:02 +0000

    updated reverse proxy rewrites

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    trailingSlash: true,
+    async rewrites() {
+      return [
+        {
+          source: "/blog/:path*/",
+          destination: "https://blog.gcsedoctor.co.uk/:path*/", // Replace with your Ghost subdomain
+        },
+        {
+          source: "/blog/:path*",
+          destination: "https://blog.gcsedoctor.co.uk/:path*", // Replace with your Ghost subdomain
+        },
+      ];
+    },
+    async headers() {
+      return [
+        {
+          source: "/blog/:path*",
+          headers: [
+            { key: "x-forwarded-host", value: "gcsedoctor.co.uk" }, // Replace with your custom domain
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
