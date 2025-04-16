ff929de2ba14a66c0de2e2564b59ca7de7a2388f
Hamza Rashid <hamzarashid720@gmail.com>
2025-04-16 16:15:44 +0100

    updated booking page

---
+++ app/bookings/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Script from 'next/script';
+import { Widget } from '@typeform/embed-react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+
+const BookingPage = () => {
+  return (
+    <>
+      {/* Google Tag Manager - Global site tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16477682494"
+        strategy="afterInteractive"
+        async
+      />
+      <Script
+        id="google-analytics-setup-redirect-page"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-16477682494');
+          `,
+        }}
+      />
+      <Widget id="Piqnx8xL" style={{ width: '100%', height: '100vh' }} />
+    </>
+  );
+};
+
+export default BookingPage;
