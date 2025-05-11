e158bf44b2fd197812553236d1b4a6dc83c79f4f
Hamza Rashid <hamzarashid720@gmail.com>
2025-05-11 19:25:30 +0100

    Update page.tsx

--- app/tutors/ruben/page.tsx
+++ app/tutors/ruben/page.tsx
@@ -41,9 +41,6 @@ const AboutPage = () => {
                 Ruben teaches:
               </Heading>
               <Stack direction='row' spacing={2} wrap='wrap' mb={4}>
-                  <Box as='span' bg='gray.200' px={2} py={1} borderRadius='md'>GCSE Biology</Box>
-                  <Box as='span' bg='gray.200' px={2} py={1} borderRadius='md'>GCSE Chemistry</Box>
-                  <Box as='span' bg='gray.200' px={2} py={1} borderRadius='md'>GCSE Physics</Box>
                   <Box as='span' bg='gray.200' px={2} py={1} borderRadius='md'>GCSE Mathematics</Box>
                   <Box as='span' bg='gray.200' px={2} py={1} borderRadius='md'>GCSE Computer Science</Box>
                   <Box as='span' bg='gray.200' px={2} py={1} borderRadius='md'>A-Level Mathematics</Box>
